f1e63492794e3088cf7b502113a82e69bf950e40
Brandon Keepers <brandon@collectiveidea.com>
2009-10-31 14:19:48 -0400

    Initial spec for mongo adapter

---
+++ lib/dm-mongo-adapter.rb
@@ -0,0 +1,7 @@
+require 'dm-core'
+require 'dm-core/adapters/abstract_adapter'
+
+module DataMapper::Adapters
+  class MongoAdapter < AbstractAdapter
+  end
+end
---
+++ spec/dm-mongo-adapter_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'dm-core/spec/adapter_shared_spec'
+
+describe DataMapper::Adapters::MongoAdapter do
+  before :all do
+    @adapter = DataMapper.setup(:default, :adapter => 'mongo')
+  end
+
+  it_should_behave_like 'An Adapter'
+end
