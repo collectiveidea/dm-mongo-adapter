13020a1f2d7ebf5629552ef41cbcbb1f19471d71
Brandon Keepers <brandon@collectiveidea.com>
2009-10-31 15:13:37 -0400

    Initial implementation of #create on adapter

--- lib/dm-mongo-adapter.rb
+++ lib/dm-mongo-adapter.rb
@@ -1,7 +1,21 @@
 require 'dm-core'
 require 'dm-core/adapters/abstract_adapter'
+require 'mongo'
 
 module DataMapper::Adapters
   class MongoAdapter < AbstractAdapter
+    def initialize(name, options = {})
+      super
+      @connection = Mongo::Connection.new
+      @database   = @connection.db(options[:database])
+    end
+    
+    def create(resources)
+      resources.each do |resource|
+        resource.id ||= Mongo::ObjectID.new.to_s
+        collection = @database.collection(resource.model.storage_name)
+        collection.save resource.attributes(:field)
+      end
+    end
   end
 end
--- spec/dm-mongo-adapter_spec.rb
+++ spec/dm-mongo-adapter_spec.rb
@@ -3,7 +3,7 @@ require 'dm-core/spec/adapter_shared_spec'
 
 describe DataMapper::Adapters::MongoAdapter do
   before :all do
-    @adapter = DataMapper.setup(:default, :adapter => 'mongo')
+    @adapter = DataMapper.setup(:default, :adapter => 'mongo', :database => 'dm-mongo-adapter')
   end
 
   it_should_behave_like 'An Adapter'
